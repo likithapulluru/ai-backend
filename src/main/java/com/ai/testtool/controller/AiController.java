eeaa86ccc61eb830cff6bef88a3a5fa8e5c69841
Likitha Pulluru <likithapulluru@gmail.com>
2025-08-03 17:46:10 -0400

    Updated Aicontroller

--- src/main/java/com/ai/testtool/controller/AiController.java
+++ src/main/java/com/ai/testtool/controller/AiController.java
@@ -5,12 +5,18 @@ import com.ai.testtool.service.OpenRouterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "https://ai-testtool.netlify.app/")
 @RestController
 @RequestMapping("/api/ai")
 public class AiController {
 
     @Autowired
     private OpenRouterService openRouterService;
+    
+    @GetMapping("/")
+    public String home() {
+        return "Backend is live";
+    }
 
     @PostMapping("/generate-test")
     public String generateTest(@RequestBody String scenario) {
